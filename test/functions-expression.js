96c6b240137fee7a2b4e29e7fc7ed3f0210556df
arshad <arshadpyaseen@gmail.com>
2025-12-03 10:59:46 +0530

    function expressions

--- test/functions-expression.js
+++ test/functions-expression.js
@@ -4,5 +4,6 @@ x = function(a) {}
 x = function(a, b) {}
 x = function(...args) {}
 x = function(a = 1) {}
-x = function(a, b = 1, ...rest) {}
-
+x = function(a, b = 1, ...rest) {
+  const a = b;
+}
