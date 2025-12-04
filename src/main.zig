b00b6a4f76bb08b649c483cbdd55ca935436f319
arshad <arshadpyaseen@gmail.com>
2025-12-05 01:26:18 +0530

    simlify main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,97 +1,71 @@
 const std = @import("std");
 const js = @import("js");
 
-const printDiagnostic = @import("print_diagnostic.zig").printDiagnostic;
-
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const file_path = "test.js";
+    const source = @embedFile("test.js");
 
-    const file = try std.fs.cwd().openFile(file_path, .{});
-    defer file.close();
+    var parser = js.Parser.init(std.heap.page_allocator, source, .{
+        .source_type = .Module,
+        .lang = .Js,
+        .is_strict = true,
+    });
 
-    var buffer: [4096]u8 = undefined;
-    var reader = file.reader(&buffer);
-    const contents = try reader.interface.allocRemaining(allocator, std.Io.Limit.limited(10 * 1024 * 1024));
-    defer allocator.free(contents);
+    var start = try std.time.Timer.start();
 
-    for (0..3) |_| {
-        var parser = js.Parser.init(std.heap.page_allocator, contents, .{});
-        var tree = try parser.parse();
-        defer tree.deinit();
-    }
+    const tree = try parser.parse();
+    defer tree.deinit();
 
-    var times = try std.ArrayList(i128).initCapacity(allocator, iterations);
-    defer times.deinit(allocator);
+    const taken = start.read();
 
-    var first_tree: ?js.ParseTree = null;
-    var first_json: ?[]u8 = null;
+    const taken_ms = @as(f64, @floatFromInt(taken)) / ns_to_ms;
 
-    for (0..iterations) |i| {
-        var parser = js.Parser.init(std.heap.page_allocator, contents, .{});
+    var line_count: usize = 1;
+        for (source) |c| {
+            if (c == '\n') line_count += 1;
+        }
 
-        const start = std.time.nanoTimestamp();
-        var tree = try parser.parse();
-        const end = std.time.nanoTimestamp();
+    const million_lines_per_sec = (@as(f64, @floatFromInt(line_count)) / 1_000_000.0) / (taken_ms / 1000.0);
 
-        try times.append(allocator, end - start);
+    const json = try js.estree.toJSON(&tree, allocator);
+    defer allocator.free(json);
 
-        if (i == 0) {
-            if (tree.hasDiagnostics()) {
-                std.debug.print("\n", .{});
-                for (tree.diagnostics.items) |diag| {
-                    printDiagnostic(contents, diag);
-                }
-                std.debug.print("\n", .{});
-            }
+    // std.debug.print("\n{s}\n", .{json});
 
-            first_json = try js.estree.toJSON(&tree, allocator);
-            first_tree = tree;
-        } else {
-            tree.deinit();
+    if (tree.hasDiagnostics()) {
+        for (tree.diagnostics.items) |err| {
+            const start_pos = getLineAndColumn(source, err.span.start);
+            const end_pos = getLineAndColumn(source, err.span.end);
+
+            std.debug.print("Error: {s} at src/test.js:{d}:{d} to src/test.js:{d}:{d}\n", .{
+                err.message,
+                start_pos.line, start_pos.col,
+                end_pos.line, end_pos.col
+            });
+            if (err.help) |help| std.debug.print("  Help: {s}", .{help});
         }
     }
 
-    var total: i128 = 0;
-    var min: i128 = times.items[0];
-    var max: i128 = times.items[0];
-
-    for (times.items) |time| {
-        total += time;
-        if (time < min) min = time;
-        if (time > max) max = time;
-    }
-
-    const avg = @divTrunc(total, iterations);
-
-    const avg_ms = @as(f64, @floatFromInt(avg)) / ns_to_ms;
-    const min_ms = @as(f64, @floatFromInt(min)) / ns_to_ms;
-    const max_ms = @as(f64, @floatFromInt(max)) / ns_to_ms;
-
-    const size_kb = @as(f64, @floatFromInt(contents.len)) / 1024.0;
-    const mb_per_sec = (size_kb / 1024.0) / (avg_ms / 1000.0);
-
-    var line_count: usize = 1;
-    for (contents) |c| {
-        if (c == '\n') line_count += 1;
-    }
-    const million_lines_per_sec = (@as(f64, @floatFromInt(line_count)) / 1_000_000.0) / (avg_ms / 1000.0);
-
-    std.debug.print("Min: {d:.3} ms | Max: {d:.3} ms | Avg: {d:.3} ms\n", .{ min_ms, max_ms, avg_ms });
-    std.debug.print("Throughput: {d:.2} MB/sec | {d:.2} million lines/sec\n", .{ mb_per_sec, million_lines_per_sec });
-
-    if (first_json) |json| {
-        defer allocator.free(json);
-        std.debug.print("\n{s}\n", .{json});
-    }
-
-    if (first_tree) |tree| {
-        tree.deinit();
-    }
+    std.debug.print("\n\n{d:.2}ms | {d:.2} million lines/sec\n\n", .{ taken_ms, million_lines_per_sec });
 }
 
-const iterations = 10;
 const ns_to_ms = 1_000_000.0;
+
+fn getLineAndColumn(source: []const u8, offset: usize) struct { line: usize, col: usize } {
+    var line: usize = 1;
+    var col: usize = 1;
+
+    for (source[0..@min(offset, source.len)]) |char| {
+        if (char == '\n') {
+            line += 1;
+            col = 1;
+        } else {
+            col += 1;
+        }
+    }
+
+    return .{ .line = line, .col = col };
+}
---
+++ src/test.js
@@ -0,0 +1,3 @@
+function cool(){
+  const nice = ""
+}
