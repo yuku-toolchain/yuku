62c3cd32de1125ee69e9785a99bb7ef6ef794b88
arshad <arshadpyaseen@gmail.com>
2025-12-09 01:44:59 +0530

    update semantic todo list

--- src/js/semantic.zig
+++ src/js/semantic.zig
@@ -15,3 +15,5 @@
 // 'evals' and 'arguments' in binding identifier and identifier reference.
 // Reserved checks: https://tc39.es/ecma262/#prod-ReservedWord
 // It is a Syntax Error if HasDirectSuper of MethodDefinition is true.
+// It is a Syntax Error if ConciseBodyContainsUseStrict of ConciseBody is true and IsSimpleParameterList of ArrowParameters is false.
+// It is a Syntax Error if any element of the BoundNames of ArrowParameters also occurs in the LexicallyDeclaredNames of ConciseBody.
