5fa9a8a297459b3a46bca921b75fdc8c668b1128
arshad <arshadpyaseen@gmail.com>
2025-12-13 21:27:20 +0530

    update

--- src/js/semantic.zig
+++ src/js/semantic.zig
@@ -32,3 +32,4 @@
 // It is a Syntax Error if this ContinueStatement is not nested, directly or indirectly (but not crossing function or static initialization block boundaries), within an IterationStatement.
 // It is a Syntax Error if any element of the BoundNames of ForDeclaration also occurs in the VarDeclaredNames of Statement.
 // It is a Syntax Error if the BoundNames of ForDeclaration contains any duplicate entries.
+// In strict mode code, functions can only be declared at top level or inside a block
