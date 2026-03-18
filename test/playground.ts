4cb03cb09d93728821e94114ecd59abe945c4195
arshad <arshadpyaseen@gmail.com>
2026-03-18 13:32:26 +0530

    update

--- test/playground.ts
+++ test/playground.ts
@@ -4,10 +4,13 @@ console.clear();
 
 console.log();
 
-const result = await parse(`function* a(){ (b = yield* c) => 1; }`, {
-	semanticErrors: true,
-});
+const result = await parse(
+	`function* a(){ (b = yield* c) => 1; } const a = cool`,
+	{
+		semanticErrors: true,
+	},
+);
 
-console.log(JSON.stringify(result.program, null, 2));
+console.log(JSON.stringify(result, null, 2));
 
 console.log();
