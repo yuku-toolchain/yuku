b7135a6313d160456033c973543b9d1234dacbd6
arshad <arshadpyaseen@gmail.com>
2026-04-15 16:10:20 +0530

    update

--- test/playground.ts
+++ test/playground.ts
@@ -2,6 +2,8 @@ import { parse } from "yuku-parser";
 
 const source = await Bun.file("test/index.ts").text();
 
-const result = parse(source);
+const result = parse(source, {
+  lang: "ts"
+});
 
 console.log(JSON.stringify(result, null, 2))
