8e5a277fcb3fbd2b08efa1a9e24a7a6b3fd22d72
arshad <arshadpyaseen@gmail.com>
2026-05-02 14:53:02 +0530

    update

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,6 @@
 const std = @import("std");
 const parser = @import("parser");
 
-std.testing.FailingAllocator
-
 pub fn main(init: std.process.Init) !void {
     const file_path = "test/index.ts";
 
